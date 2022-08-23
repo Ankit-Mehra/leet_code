cf7cf48399ec9185d522db717da552f662ea4322
Ankit Mehra <ankitmehra1986@gmail.com>
2022-08-23 19:56:56 -0400

    contains duplicate another way

--- containsDuplicate.py
+++ containsDuplicate.py
@@ -1,17 +1,26 @@
+"""Leet Code Practice"""
+
 def contains_duplicate(nums:list[int]) -> bool:
-    """Given an integer array nums, return true if any value 
+    """Given an integer array nums, return true if any value
     appears at least twice in the array, and return false if every element is distinct."""
     check = set(nums)
     return len(check) < len(nums)
-    
+ 
 def contains_duplicate2(nums:list[int])-> bool:
-    """Given an integer array nums, return true if any value 
+    """Given an integer array nums, return true if any value
     appears at least twice in the array, and return false if every element is distinct."""
-    pass
+    hashset = set()
+    
+    for num in nums:
+        if num in hashset:
+            return True
+        hashset.add(num)
+    return False
 
 def main():
-    nums = [1,2,3,1]
-    print(contains_duplicate(nums))
+    """main function"""
+    nums = [1,2,3,4]
+    print(contains_duplicate2(nums))
 
 if __name__ == "__main__":
     main()
